5b660575cc004078cde3167d5407b6785fb41f89
WIL PANNELL <wgpannel@us.ibm.com>
2017-12-13 08:58:13 -0800

    pass the first test

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,9 +1,14 @@
+let fahrenheit2celsius = () => 0;
+
 describe.only('a fahrenheit2celsius feature', () => {
   it('has a working test infrastructure', () => {
     true.should.be.true();
   });
 
-  it('fahrenheit2celsius(32) === 0');
+  it('fahrenheit2celsius(32) === 0', () => {
+    fahrenheit2celsius(32).should.equal(0);
+  });
+
   it('fahrenheit2celsius(212) === 100');
   it('fahrenheit2celsius(50) === 10');
 });
